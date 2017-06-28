ebb5f1aa7a7b868c1f1d9b027514887296f362cd
Yu Zhai <me@zhaiyusci.net>
2017-06-28 10:36:01 +0800

    modified:   qho.f

--- qho.f
+++ qho.f
@@ -22,7 +22,10 @@
         real*8 :: x, psi
         integer :: n, f
 
-        ! Ref: Bernath Spectr. Atom. Mol. P215
+        ! Ref: Bernath Spectr. Atom. Mol. P.215
+        ! |Psi_n(x)=(2^n*n!)^(-1/2)\sqrt[4]{\alpha/\pi}
+        ! \times e^{-\alpha*x^2/2}*H_n(\sqrt{\alpha}x)
+        ! where \alpha=\mu\omega/\hbar
         call hermitepoly(n,alpha**0.5d0*x,psi)
         !psi=1.0d0
         !write(*,*) "Hn(x)", psi
@@ -34,6 +37,10 @@
       end subroutine harmonicwf
 
       subroutine hermitepoly(n,x,h)
+        ! When coding this guy
+        ! I keep in mind that
+        ! The coeffcients are only calculated when they are in need
+        ! and the calculated ones are stored for next time
         implicit none
         integer :: a(0:999,0:999)=0
         ! Store the coefficients of Hermite Polynomial
@@ -43,13 +50,24 @@
         integer :: i,j
         real*8 :: x, h
         save oldn, a
+        ! Initialize the leading terms
         a(0,0)=1
         a(1,1)=2
+        ! Following the recursion relation on Wikipedia:
+        ! H_{n+1}(x)=2xH_n(x)-2nH_{n-1}(x)
+        ! Only the coefficients are calculated
+        
         if(n.gt.oldn.and.n.gt.1)then
           do i=oldn+1 ,n
+          ! The highest order of H_i is i and it is easy to find that
+          ! to preserve the symmetry of the harmonic oscillator
+          ! only even or odd function here
           do j=i,1,-2
           a(j,i)=2*a(j-1,i-1)-2*(i-1)*a(j,i-2)
           end do
+          ! For the zero order
+          ! For odd ones, it is not harmful to calculate the zero order
+          ! coefficient
           a(0,i)=-2*(i-1)*a(0,i-2)
           end do
           oldn=n
@@ -65,6 +83,10 @@
           
 
       subroutine factorial(n,f)
+        ! When coding this guy
+        ! I keep in mind that
+        ! The results are only calculated when they are in need
+        ! and the calculated ones are stored for next time
         implicit none
         integer :: a(0:999)=1
         integer :: n, f
@@ -74,12 +96,9 @@
 
         a(0)=1
         if(n.gt.oldn.and.n.gt.0)then
-          !write(*,*) "more", n
           do i=oldn+1 ,n
           a(i)=i*a(i-1)
-          !write(*,*) i, a(i)
           end do
-          !return
           oldn=n
         end if
 
